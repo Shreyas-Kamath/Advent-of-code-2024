3903f90e9f027f775dbeaa9d787f7b20834604e0
Shreyas Kamath <shreyass29122001@gmail.com>
2024-12-11 17:11:39 +0530

    new pattern

--- day3/solution part 2.py
+++ day3/solution part 2.py
@@ -3,14 +3,14 @@ import re
 with open('data.txt', 'r') as file:
     content = file.read()
 
-# pattern = r"mul\((\d+),(\d+)\)"
-pattern = r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))"
+pattern = r"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))"
 
 enable_multiplication = True
  
 sum = 0
 
 data = re.findall(pattern, content)
+
 for pair in data:
     match pair[0]:
         case "do()":
